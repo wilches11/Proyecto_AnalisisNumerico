4cf83b420d483dead03956ae01150e407b7a770c
berioss <68913258+berioss@users.noreply.github.com>
2022-05-30 22:37:25 -0500

    Add Relative Error

--- Metodos/Newton.py
+++ Metodos/Newton.py
@@ -15,9 +15,10 @@ def Newton(f,x0,tol,Nmax):
         xact = xant-fant/(sympy.diff(f,x).subs(x,xant))
         fact = f.subs(x,xant)
         E = abs(xact-xant)
+        Er = abs(E/xact)
         cont = cont + 1
         xant = xact
         fant = fact
-    return [float(xact),cont,float(E)]
+    return [float(xact),cont,float(E),float(Er)]
 
 #print(Newton(f,-10,0.001,5))
