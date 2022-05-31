c9f6e4312ce903f060f3b2ec1158e1c5cf4aef06
berioss <68913258+berioss@users.noreply.github.com>
2022-05-30 23:14:47 -0500

    Update Newton.py

--- Metodos/Newton.py
+++ Metodos/Newton.py
@@ -2,23 +2,26 @@ from symtable import Symbol
 import sympy
 
 x = sympy.Symbol("x")
-f = ((x-4)**2)-100
+f = x**2 - 2
 
 def Newton(f,x0,tol,Nmax):
 
-    xant = 0
+    xant = x0
     fant = f.subs(x,xant)
     E = 1000
     cont = 0
 
     while E > tol and cont < Nmax :
-        xact = xant-fant/(sympy.diff(f,x).subs(x,xant))
-        fact = f.subs(x,xant)
+        df = sympy.diff(f,x)
+        temp = df.subs(x,xant)
+        xact = xant-fant/temp
+        fact = f.subs(x,xact)
         E = abs(xact-xant)
         Er = abs(E/xact)
         cont = cont + 1
         xant = xact
         fant = fact
     return [float(xact),cont,float(E),float(Er)]
+
 
 #print(Newton(f,-10,0.001,5))
