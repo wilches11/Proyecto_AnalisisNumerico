b64b04f4c6a5cdf530179e998e2cbf22d362829e
berioss <68913258+berioss@users.noreply.github.com>
2022-05-30 22:36:06 -0500

    Add Relative Error

--- Metodos/PuntoFijo.py
+++ Metodos/PuntoFijo.py
@@ -4,31 +4,6 @@ import sympy
 x = sympy.Symbol("x")
 f = (x)**2 - 100
 
-def Cre_o_Decre(f,x0):
-    return sympy.diff(f,x).subs(x,x0) > 0
-
-def PuntoFijo_f(f,x0,tol,Nmax):
-
-    if Cre_o_Decre(f,x0):
-        f = f + 1/(x-1)
-        g = 1/f + 1
-    else :
-        f = -f + 1/(x-1)
-        g = 1/f + 1
-
-    #Inicialización
-    xant = x0
-    E = 1000
-    cont = 0
-
-    #Ciclo
-    while E > tol and cont < Nmax :
-        xact = g.subs(x,xant)
-        E = abs(xact-xant)
-        cont = cont + 1
-        xant = xact
-    return [xact,cont,E]
-
 def PuntoFijo(g,x0,tol,Nmax):
 
     #Inicialización
@@ -40,8 +15,9 @@ def PuntoFijo(g,x0,tol,Nmax):
     while E > tol and cont < Nmax :
         xact = g.subs(x,xant)
         E = abs(xact-xant)
+        Er = abs(E/xact)
         cont = cont + 1
         xant = xact
-    return [xact,cont,E]
+    return [xact,cont,E,Er]
 
 print(PuntoFijo_f(f,-12,0.01,100))
